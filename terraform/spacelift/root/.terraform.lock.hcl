0e66d602f8bbaf0f1e285e78f94389981ba122f4
Jace Tan <jaceys.tan@gmail.com>
2026-05-24 22:19:58 +0100

    Create Spacelift root stack (#2)

---
+++ terraform/spacelift/root/.terraform.lock.hcl
@@ -0,0 +1,21 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/spacelift-io/spacelift" {
+  version     = "1.49.0"
+  constraints = "~> 1.49"
+  hashes = [
+    "h1:/rOwb8fjbpqbIPB6DdJ4vZzjiSGNMw57F/DkMMz3DFY=",
+    "h1:GvGvB5yIYGAx4kPhL/o4rKwBmld454PDvf8HH6sm4kQ=",
+    "h1:YEnQkzIpttE/W1Y46aZlk3rZoyD2Tcmwcf3Pe2YcSww=",
+    "h1:kWOk2RtW++4Yyu2Oc3vCoMBAvB/vbyb3+wSSOxGvAzg=",
+    "h1:mctrQ6fI3yjCciMT7Y7tKqQwV/vOqWmKWJwWG7opVeU=",
+    "h1:nYOLtukCRFFncrVJUq2yVN2NETFYoJh3d3FYm+yxWts=",
+    "zh:012c3db88af127138bb761b6bb1f989c888e1041393d4b3f4124c96bd8c667dc",
+    "zh:1d90d47971c7f794a031e376e9d9685ebf322e349d576443d14b44d3b6a1700b",
+    "zh:50bd0d94232f32b6455e4735f00cb493a924419398de2693d2ac48aa74d33509",
+    "zh:bc0d8f006dbe567f551a8d0324989d335e99fd1b5aa84f62a5f335fbeb728b34",
+    "zh:ce4b7fc22a9767e8d2ecfd53e3b02e562cf12c82c30b55a62481db6063f2fad1",
+    "zh:ffc615a5746f5b77786e129296e35360ee48e877df6d1d2593123be5bd08d85d",
+  ]
+}
---
+++ terraform/spacelift/root/oci.tf
@@ -0,0 +1,63 @@
+resource "spacelift_stack" "oci_root" {
+  space_id                         = data.spacelift_space.root.id
+  name                             = "oci-root"
+  description                      = "☁️ OCI root"
+  repository                       = "homelab"
+  branch                           = "main"
+  project_root                     = "terraform/core/oci"
+  additional_project_globs         = ["terraform/environments/**/cell.json"]
+  terraform_workflow_tool          = "OPEN_TOFU"
+  terraform_version                = "1.12.0"
+  autodeploy                       = true
+  enable_local_preview             = true
+  protect_from_deletion            = true
+  terraform_smart_sanitization     = true
+  enable_well_known_secret_masking = true
+
+  labels = [
+    "feature:add_plan_pr_comment",
+    "terraform-provider-oci",
+    "sops-enabled",
+  ]
+}
+
+resource "spacelift_stack_dependency" "oci_root" {
+  stack_id            = spacelift_stack.oci_root.id
+  depends_on_stack_id = data.spacelift_stack.spacelift_root.id
+}
+
+resource "spacelift_environment_variable" "oci_root_region" {
+  stack_id   = spacelift_stack.oci_root.id
+  name       = "TF_VAR_oci_region"
+  value      = "uk-london-1"
+  write_only = false
+}
+
+resource "spacelift_context" "terraform_provider_oci" {
+  space_id = data.spacelift_space.root.id
+  name     = "terraform-provider-oci"
+
+  labels = ["autoattach:terraform-provider-oci"]
+}
+
+resource "spacelift_mounted_file" "oci_api_key" {
+  context_id         = spacelift_context.terraform_provider_oci.id
+  relative_path      = ".oci/oci_api_key.pem"
+  content_wo         = base64encode(local.secrets.oci["oci_api_key.pem"])
+  content_wo_version = sha256(local.secrets.oci["oci_api_key.pem"])
+  write_only         = true
+}
+
+resource "spacelift_environment_variable" "terraform_provider_oci" {
+  for_each = {
+    tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaapg6qcdugjc6ya3cas7ddz2dijh3oqhr2pmfhel2qtmyfir7vfcq"
+    user_ocid        = "ocid1.user.oc1..aaaaaaaapafsajfep23hgc2fju6id7q6hydzvc2phzb7sjmp2iqhub57rula"
+    fingerprint      = "7c:55:87:8e:c2:ef:75:cd:aa:56:8b:56:8a:35:47:56"
+    private_key_path = "/mnt/workspace/${spacelift_mounted_file.oci_api_key.relative_path}"
+  }
+
+  context_id = spacelift_context.terraform_provider_oci.id
+  name       = "TF_VAR_oci_${each.key}"
+  value      = each.value
+  write_only = false
+}
