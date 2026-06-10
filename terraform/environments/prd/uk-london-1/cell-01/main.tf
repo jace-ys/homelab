edf770540f5b39fa9c258c2e9297887eccd0fa9b
jace-ys <jaceys.tan@gmail.com>
2026-06-10 01:02:05 +0100

    Create prd network

--- terraform/environments/prd/uk-london-1/cell-01/main.tf
+++ terraform/environments/prd/uk-london-1/cell-01/main.tf
@@ -1,3 +1,17 @@
 data "oci_identity_compartment" "cell" {
   id = var.cell_compartment_ids[var.cell_id]
 }
+
+data "oci_identity_availability_domains" "all" {
+  compartment_id = var.oci_tenancy_ocid
+}
+
+locals {
+  ads = data.oci_identity_availability_domains.all.availability_domains
+  fds = ["FAULT-DOMAIN-1", "FAULT-DOMAIN-2", "FAULT-DOMAIN-3"]
+
+  vcn_cidr                = "10.0.0.0/16"
+  lb_external_subnet_cidr = "10.0.0.0/24"
+  lb_internal_subnet_cidr = "10.0.1.0/24"
+  k3s_subnet_cidr         = "10.0.2.0/24"
+}
