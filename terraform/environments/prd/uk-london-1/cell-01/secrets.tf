10df87dc43213856a08f592be217befb89aa368c
Jace Tan <jaceys.tan@gmail.com>
2026-06-15 23:56:44 +0100

    Rename vault and secret (#39)

--- terraform/environments/prd/uk-london-1/cell-01/secrets.tf
+++ terraform/environments/prd/uk-london-1/cell-01/secrets.tf
@@ -1,12 +1,12 @@
 resource "oci_kms_vault" "k3s" {
   compartment_id = data.oci_identity_compartment.cell.id
-  display_name   = replace(local.k3s_cluster_name, ".", "-")
+  display_name   = "k3s"
   vault_type     = "DEFAULT"
 }
 
 resource "oci_kms_key" "k3s" {
   compartment_id      = data.oci_identity_compartment.cell.id
-  display_name        = replace(local.k3s_cluster_name, ".", "-")
+  display_name        = "k3s"
   management_endpoint = oci_kms_vault.k3s.management_endpoint
   protection_mode     = "SOFTWARE"
 
@@ -20,7 +20,7 @@ resource "oci_vault_secret" "cloudflare_api_token" {
   compartment_id = data.oci_identity_compartment.cell.id
   vault_id       = oci_kms_vault.k3s.id
   key_id         = oci_kms_key.k3s.id
-  secret_name    = "cloudflare_api_token"
+  secret_name    = "cloudflare-api-token"
 
   secret_content {
     content_type = "BASE64"
