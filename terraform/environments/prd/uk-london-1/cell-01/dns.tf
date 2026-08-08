0ad579f822abe9fd94604afa6a4755c2353f7e6d
jace-ys <jaceys.tan@gmail.com>
2026-08-08 21:29:33 +0100

    Enable Cloudflare proxy for *.jaceystan.com

--- terraform/environments/prd/uk-london-1/cell-01/dns.tf
+++ terraform/environments/prd/uk-london-1/cell-01/dns.tf
@@ -12,6 +12,6 @@ resource "cloudflare_dns_record" "internal" {
   name    = "*.${var.base_domain_internal}"
   type    = "A"
   content = oci_core_public_ip.lb_internal.ip_address
-  proxied = false
+  proxied = true
   ttl     = 3600
 }
